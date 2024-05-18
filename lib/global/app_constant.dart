62321bca336c5473d08715f3a559db2b1116ddfd
reypryma <refanopradana@gmail.com>
2024-05-18 20:19:00 +0700

    init commit

---
+++ lib/global/app_constant.dart
@@ -0,0 +1,9 @@
+class AppConstant {
+  static const _imageAssets = "assets/images";
+  static const logoImage = "$_imageAssets/logo.png";
+  static const profileAvatar = "$_imageAssets/profile_avatar.jpg";
+  static const likeImage = "$_imageAssets/like.png";
+  static const chatImage = "$_imageAssets/chat.png";
+  static const favoriteImage = "$_imageAssets/favorite.png";
+
+}
