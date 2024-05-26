9976cfaf0562a83d45da83e4a0443ae6903acdac
reypryma <refanopradana@gmail.com>
2024-05-26 21:52:51 +0700

    add auth for register

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,7 +1,14 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tinker_clone/global/app_constant.dart';
+
+import '../models/Person.dart' as person_model;
+import '../screens/home/home_screen.dart';
 
 class AuthController extends GetxController {
   static AuthController authController = Get.find();
@@ -33,4 +40,107 @@ class AuthController extends GetxController {
 
     pickedFile = Rx<File?>(File(imageFile!.path));
   }
+
+  Future<String> uploadImageToStorage(File imageProfile) async{
+    Reference referenceStorage = FirebaseStorage.instance.ref()
+        .child(AppConstant.firebaseProfileImages)
+        .child(FirebaseAuth.instance.currentUser!.uid);
+
+    UploadTask task = referenceStorage.putFile(imageProfile);
+    // Uploading task using snapshot, await to get download url
+    TaskSnapshot snapshot = await task;
+
+    String downloadUrl = await snapshot.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  createNewUserAccount(
+      //personal info
+      File imageProfile, String email, String password,
+      String name, String age,
+      String phoneNo, String city,
+      String country, String profileHeading,
+      String lookingForInaPartner,
+
+      //Appearance
+      String height, String weight, String bodyType,
+
+      //Life style
+      String drink, String smoke, String martialStatus,
+      String haveChildren, String noOfChildren, String profession,
+      String employmentStatus, String income, String livingSituation,
+      String willingToRelocate, String relationshipYouAreLookingFor,
+
+      //Background - Cultural Values
+      String nationality, String education, String languageSpoken,
+      String religion, String ethnicity,) async
+  {
+    try
+    {
+      //1. authenticate user and create User With Email and Password
+      UserCredential credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email,
+          password: password
+      );
+
+      //2. upload image to storage
+      String urlOfDownloadedImage = await uploadImageToStorage(imageProfile);
+
+      //3. save user info to firestore database
+      person_model.Person personInstance = person_model.Person(
+
+        //personal info
+        imageProfile: urlOfDownloadedImage,
+        email: email,
+        password: password,
+        name: name,
+        age: int.parse(age),
+        phoneNo: phoneNo,
+        city: city,
+        country: country,
+        profileHeading: profileHeading,
+        lookingForInaPartner: lookingForInaPartner,
+        publishedDateTime: DateTime.now().millisecondsSinceEpoch,
+
+        //Appearance
+        height: height,
+        weight: weight,
+        bodyType: bodyType,
+
+        //Life style
+        drink: drink,
+        smoke: smoke,
+        martialStatus: martialStatus,
+        haveChildren: haveChildren,
+        noOfChildren: noOfChildren,
+        profession: profession,
+        employmentStatus: employmentStatus,
+        income: income,
+        livingSituation: livingSituation,
+        willingToRelocate: willingToRelocate,
+        relationshipYouAreLookingFor: relationshipYouAreLookingFor,
+
+        //Background - Cultural Values
+        nationality: nationality,
+        education: education,
+        languageSpoken: languageSpoken,
+        religion: religion,
+        ethnicity: ethnicity,
+
+      );
+
+      await FirebaseFirestore.instance.collection(AppConstant.firebaseUserCollections)
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .set(personInstance.toJson());
+
+      Get.snackbar("Account Created", "Congratulations, your account has been created.");
+      Get.to(HomeScreen());
+    }
+    catch(errorMsg)
+    {
+      Get.snackbar("Account Creation Unsuccessful", "Error occurred: $errorMsg");
+    }
+  }
+
+
 }
