f7b07ec46013538a0abd49f5cb7b3da826717285
reypryma <refanopradana@gmail.com>
2024-06-19 07:57:35 +0700

    add view sent controller

--- lib/ui/screens/home/fragments/view_sent_fragment.dart
+++ lib/ui/screens/home/fragments/view_sent_fragment.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../global/app_constant.dart';
 
 class ViewSentViewReceivedFragment extends StatefulWidget {
   const ViewSentViewReceivedFragment({super.key});
@@ -8,17 +12,236 @@ class ViewSentViewReceivedFragment extends StatefulWidget {
 }
 
 class _ViewSentViewReceivedFragmentState extends State<ViewSentViewReceivedFragment> {
+  bool isViewSentClicked = true;
+  List<String> viewSentList = [];
+  List<String> viewReceivedList = [];
+  List viewsList = [];
+
+  getViewsListKeys() async
+  {
+    if(isViewSentClicked)
+    {
+      var viewSentDocument = await FirebaseFirestore.instance
+          .collection(AppConstant.firebaseUserCollections)
+          .doc(currentUserID.toString()).collection(AppConstant.firebaseUserViewSentCollections)
+          .get();
+
+      for(int i=0; i<viewSentDocument.docs.length; i++)
+      {
+        viewSentList.add(viewSentDocument.docs[i].id);
+      }
+
+      if (kDebugMode) {
+        print("viewSentList = $viewSentList");
+      }
+      getKeysDataFromUsersCollection(viewSentList);
+    }
+    else
+    {
+      var viewReceivedDocument = await FirebaseFirestore.instance
+          .collection(AppConstant.firebaseUserCollections)
+          .doc(currentUserID.toString()).collection(AppConstant.firebaseUserViewReceivedCollections)
+          .get();
+
+      for(int i=0; i<viewReceivedDocument.docs.length; i++)
+      {
+        viewReceivedList.add(viewReceivedDocument.docs[i].id);
+      }
+
+      if (kDebugMode) {
+        print("viewReceivedList = $viewReceivedList");
+      }
+      getKeysDataFromUsersCollection(viewReceivedList);
+    }
+  }
+
+  getKeysDataFromUsersCollection(List<String> keysList) async
+  {
+    var allUsersDocument = await FirebaseFirestore.instance.collection("users").get();
+
+    for(int i=0; i<allUsersDocument.docs.length; i++)
+    {
+      for(int k=0; k<keysList.length; k++)
+      {
+        if(((allUsersDocument.docs[i].data() as dynamic)["uid"]) == keysList[k])
+        {
+          viewsList.add(allUsersDocument.docs[i].data());
+        }
+      }
+    }
+
+    setState(() {
+      viewsList;
+    });
+
+    if (kDebugMode) {
+      print("viewsList = $viewsList");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getViewsListKeys();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "View Sent View Received Fragment",
-          style: TextStyle(
-            color: Colors.green,
-            fontSize: 20,
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            TextButton(
+              onPressed: () {
+                viewSentList.clear();
+                viewSentList = [];
+                viewReceivedList.clear();
+                viewReceivedList = [];
+                viewsList.clear();
+                viewsList = [];
+
+                setState(() {
+                  isViewSentClicked = true;
+                });
+
+                getViewsListKeys();
+              },
+              child: Text(
+                "Profile I Viewed",
+                style: TextStyle(
+                  color: isViewSentClicked ? Colors.white : Colors.grey,
+                  fontWeight: isViewSentClicked ? FontWeight.bold : FontWeight
+                      .normal,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+
+            const Text(
+              "   |   ",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+
+            TextButton(
+              onPressed: () {
+                viewSentList.clear();
+                viewSentList = [];
+                viewReceivedList.clear();
+                viewReceivedList = [];
+                viewsList.clear();
+                viewsList = [];
+
+                setState(() {
+                  isViewSentClicked = false;
+                });
+
+                getViewsListKeys();
+              },
+              child: Text(
+                "Viewed My Profile",
+                style: TextStyle(
+                  color: isViewSentClicked ? Colors.grey : Colors.white,
+                  fontWeight: isViewSentClicked ? FontWeight.normal : FontWeight
+                      .bold,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ],
         ),
+        centerTitle: true,
+      ),
+      body: viewsList.isEmpty
+          ? const Center(
+        child: Icon(Icons.person_off_sharp, color: Colors.white, size: 60,),
+      )
+          : GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(8),
+        children: List.generate(viewsList.length, (index) {
+          return GridTile(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Card(
+                color: Colors.blue.shade200,
+                child: GestureDetector(
+                  onTap: () {
+
+                  },
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            viewsList[index]["imageProfile"],),
+                          fit: BoxFit.cover,
+                        )
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+
+                            const Spacer(),
+
+                            //name - age
+                            Text(
+                              "${viewsList[index]["name"]} ◉ ${viewsList[index]["age"]}",
+                              maxLines: 2,
+                              style: const TextStyle(
+                                overflow: TextOverflow.ellipsis,
+                                color: Colors.grey,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            const SizedBox(
+                              height: 4,
+                            ),
+
+                            //icon - city - country
+                            Row(
+                              children: [
+
+                                const Icon(
+                                  Icons.location_on_outlined,
+                                  color: Colors.grey,
+                                  size: 16,
+                                ),
+
+                                Expanded(
+                                  child: Text(
+                                    "${viewsList[index]["city"]}, ${viewsList[index]["country"]}",
+                                    maxLines: 2,
+                                    style: const TextStyle(
+                                      overflow: TextOverflow.ellipsis,
+                                      color: Colors.grey,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
